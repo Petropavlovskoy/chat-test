d1aadfc10082f0946f4c96c6d4b881a48875c096
Sergio <sergio033032@gmail.com>
2021-05-30 20:13:36 +0300

    com 2
    com2

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+// Подключение всех модулей к программе
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+// Отслеживание url адреса и отображение нужной HTML страницы
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-//обработка данных на сервере, возрат
+
+// Функция, которая сработает при подключении к странице
+// Считается как новый пользователь
 io.on('connection', (socket) => {
   socket.on('chat message', (data) => {
+
     io.emit('chat message',{
 		message: data.message,
 		name: data.name
+
    });
   });
 });
+
+// Отслеживание порта
 http.listen(port)
