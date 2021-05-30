d0fd768891c9345f4b1c0658b182221dbaafa383
Sergio <sergio033032@gmail.com>
2021-05-30 19:55:47 +0300

    com 1
    com 1

--- index.js
+++ index.js
@@ -3,21 +3,16 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
 //обработка данных на сервере, возрат
 io.on('connection', (socket) => {
   socket.on('chat message', (data) => {
     io.emit('chat message',{
 		message: data.message,
 		name: data.name
-		
-	});
+   });
   });
-
 });
-
 http.listen(port)
