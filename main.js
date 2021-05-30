3936c37e7ef88495da511492de51dba9e41e1a8d
Sergio <sergio033032@gmail.com>
2021-05-30 19:44:20 +0300

    chat
    chat

---
+++ main.js
@@ -0,0 +1,21 @@
+const socket = io();
+const form = document.querySelector(".form");
+const messages = document.querySelector(".messages");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const nameBlock = document.querySelector(".name");
+
+const userName = prompt("Ваше имя");
+nameBlock.innerHTML = `${userName}`;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input.value) {
+    socket.emit("chat message", {
+      message: input.value,
+      name: userName,
+    })
+    input.value = ''
+  }
+});
